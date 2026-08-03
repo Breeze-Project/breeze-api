98934e778e50b55f2abab8573d1313ea6b7caf9f
hxragi <mixintrace@gmail.com>
2026-08-04 00:36:03 +0300

    fix: auto unsubscribe from events and command when module disable

--- src/main/java/org/breeze/api/event/EventBus.java
+++ src/main/java/org/breeze/api/event/EventBus.java
@@ -1,5 +1,6 @@
 package org.breeze.api.event;
 
+import org.breeze.api.module.BreezeModule;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Consumer;
@@ -16,6 +17,21 @@ public interface EventBus {
             @NotNull Class<T> eventType,
             @NotNull EventPriority priority,
             @NotNull Consumer<T> handler);
+
+    @NotNull
+    <T extends BreezeEvent> Subscription subscribe(
+            @NotNull BreezeModule module,
+            @NotNull Class<T> eventType,
+            @NotNull Consumer<T> handler);
+
+    @NotNull
+    <T extends BreezeEvent> Subscription subscribe(
+            @NotNull BreezeModule module,
+            @NotNull Class<T> eventType,
+            @NotNull EventPriority priority,
+            @NotNull Consumer<T> handler);
+
+    void unsubscribeAll(@NotNull BreezeModule module);
 
     <T extends BreezeEvent> void publish(@NotNull T event);
 }
