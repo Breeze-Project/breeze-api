8fde1dfb0a9859afa314f3b54e2bae3908017d0a
hxragi <mixintrace@gmail.com>
2026-08-04 02:10:41 +0300

    fix(commands): unchecked case

--- src/main/java/org/breeze/api/command/CommandService.java
+++ src/main/java/org/breeze/api/command/CommandService.java
@@ -1,12 +1,15 @@
 package org.breeze.api.command;
 
-import com.mojang.brigadier.tree.LiteralCommandNode;
 import org.breeze.api.module.BreezeModule;
 import org.jetbrains.annotations.NotNull;
 
+import com.mojang.brigadier.tree.LiteralCommandNode;
+
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+
 public interface CommandService {
 
-    void register(@NotNull BreezeModule module, @NotNull LiteralCommandNode<?> command);
+    void register(@NotNull BreezeModule module, @NotNull LiteralCommandNode<CommandSourceStack> command);
 
     void unregisterAll(@NotNull BreezeModule module);
 }
